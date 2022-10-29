3076fbffdbbeebf42e989a5a6becb2f81388bb8e
Denis Belozerov <molotov.ant@mail.ru>
2022-10-30 01:09:44 +0500

    fix

--- src/pages/register/Page4.jsx
+++ src/pages/register/Page4.jsx
@@ -1,13 +1,22 @@
-export default function Page4({result}) {
-    return (
-        <div className={"wrapper"}>
-            <h1 className={"header"}>Анкета</h1>
-            <div className={"form"}>
-                <label className={"form-label"}>Мои хобби</label>
-                <select className={"input"}></select>
-                <label className={"form-label"}>Ищу коллег только из своего города</label>
-                <input type={"button"} className={"input"}></input>
-            </div>
-            <button className={"btn"} onClick={props.next}>Готово</button>
-        </div>);
+export default function Page4({ result }) {
+  function postResult() {
+    //doPost
+    console.log(result);
+  }
+  return (
+    <div className={"wrapper"}>
+      <h1 className={"header"}>Анкета</h1>
+      <div className={"form"}>
+        <label className={"form-label"}>Мои хобби</label>
+        <select className={"input"}></select>
+        <label className={"form-label"}>
+          Ищу коллег только из своего города
+        </label>
+        <input type={"button"} className={"input"}></input>
+      </div>
+      <button className={"btn"} onClick={postResult}>
+        Готово
+      </button>
+    </div>
+  );
 }
