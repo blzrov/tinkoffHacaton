36bec14e8f334b80aefbc5bcd8982d530c162a57
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-10-30 02:22:03 +0500

    Add interests

--- src/pages/register/Page4.jsx
+++ src/pages/register/Page4.jsx
@@ -3,12 +3,20 @@ export default function Page4({ result }) {
     //doPost
     console.log(result.current);
   }
+  result.current.hobbies = []
   return (
     <div className={"wrapper"}>
       <h1 className={"header"}>Анкета</h1>
       <div className={"form"}>
         <label className={"form-label"}>Мои хобби</label>
-        <select className={"input"}></select>
+          <ul className="interests">
+              {["Спорт", "Компьютерные игры", "Настольные игры", "Музыка"].map((element, index) => {
+                  return <button onClick={(e) => {
+                      e.target.disabled = true;
+                      result.current.hobbies.push(e.target.textContent);
+                  }} className={"hobby"} key={index}>{element}</button>
+              })}
+          </ul>
         <label className={"form-label"}>
           Ищу коллег только из своего города
         </label>
