4eaf7e06d11c69131b30da0bd86875c0a0af4dd5
Denis Belozerov <molotov.ant@mail.ru>
2022-10-29 13:23:38 +0500

    add page1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Page1 from "./pages/register/Page1.jsx";
 
 function App() {
   return (
     <div className="App">
-123
+      <Page1></Page1>
     </div>
   );
 }
