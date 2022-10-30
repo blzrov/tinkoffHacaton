fc978e74eed9c3498ffbd084e2cd675237041b63
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-10-30 08:36:07 +0500

    Fix

--- src/pages/register/Page2.jsx
+++ src/pages/register/Page2.jsx
@@ -19,11 +19,24 @@ export default function Page2({ next, result }) {
           className={"input"}
         ></input>
         <label className={"form-label"}>Пол</label>
-        <input
-          onChange={(e) => (result.current.user_sex = e.target.value)}
-          type={"button"}
-          className={"input"}
-        ></input>
+        <fieldset className="form-sex">
+            <input
+              onChange={(e) => (result.current.user_sex = e.target.value)}
+              type={"radio"}
+              className={"input"}
+              value={"Men"}
+              id="radio-1"
+            ></input>
+            <label htmlFor="radio-1">М</label>
+            <input
+                onChange={(e) => (result.current.user_sex = e.target.value)}
+                type={"radio"}
+                className={"input"}
+                value={"Women"}
+                id="radio-2"
+            ></input>
+            <label htmlFor="radio-2">Ж</label>
+        </fieldset>
         <label className={"form-label"}>Возраст</label>
         <input
           onChange={(e) => (result.current.user_age = e.target.value)}
